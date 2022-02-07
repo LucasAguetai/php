3862c935ff9d3235314ff8be7bdef17d1a0080f5
LucasAguetai <lucas.aguetai@icloud.com>
2022-02-07 13:15:24 +0100

    New chapter

--- learning.php
+++ learning.php
@@ -1,8 +1,6 @@
 <?php
-$heure = (int)readline("Entrez une heure : ");
-if ((9 <= $heure && $heure < 12) || (14 <= $heure && $heure < 19)){
-echo "Le magasin est ouvert";
-} else {
-echo "Le magasin est fermé";
+$notes = [10, 12, 16];
+foreach ($notes as $note) {
+	echo " - $note\n";
 }
 ?>
